6c6527ff44ced32235ebe627cfbabc8adfe3ca1f
agbossi <abossi@itba.edu.ar>
2020-05-16 21:37:56 -0300

    xor broken

--- src/ar/edu/itba/sia/group3/Main.py
+++ src/ar/edu/itba/sia/group3/Main.py
@@ -8,16 +8,18 @@ import ar.edu.itba.sia.group3.Functions.Activation_Functions as af
 
 # or_function = [['x', 'y', 'or'], [1, 1, 1], [1, 0, 1], [0, 1, 1], [0, 0, 0]]
 and_function = [['x', 'y', 'or'], [1, 0, 0], [0, 1, 0], [0, 0, 0], [1, 1, 1]]
-# xor_function = [['x', 'y', 'or'], [1, 1, 0], [0, 0, 0], [1, 0, 1], [0, 1, 1]]
+xor_function = [['x', 'y', 'or'], [1, 1, 0], [0, 0, 0], [1, 0, 1], [0, 1, 1]]
 and_data_set = np.array(and_function[1:])
+xor_data_set = np.array(xor_function[1:])
+
 activation_function = af.StepFunction()
 features = 2
 iteration_limit = 10
 restart_condition = 5
-learning_rate = 0.2
+learning_rate = 0.9
 
 p = md.Perceptron(features, activation_function, "classification")
-trained_weights, errors_per_epoch = p.batch_training(and_data_set, learning_rate, restart_condition, iteration_limit)
+trained_weights, errors_per_epoch = p.batch_training(xor_data_set, learning_rate, restart_condition, iteration_limit)
 mtr.converge_metric(iteration_limit, errors_per_epoch)  # exploto porque errors aparecio con longitud 11
 p.test_perceptron(and_data_set)
 
